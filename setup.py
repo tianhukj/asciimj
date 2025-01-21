8d30ba4a7c2b12fa8784d3fc78f175edc38b19fa
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-21 12:29:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name="image-ascii",
     version="0.1.0",
-    author="Tianhukj",
+    author="tianhukj",
     author_email="tianhukj@outlook.com",
-    description="A simple tool to convert images to ASCII art",
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown/ascii",
+    description="A package to convert images to ASCII art",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/tianhukj/image-ascii",
     packages=find_packages(),
     install_requires=[
         "Pillow",
     ],
-    entry_points={
-        'console_scripts': [
-            'image_ascii=image-ascii.image-ascii:convert_image_to_ascii',
-        ],
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
