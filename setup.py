1d9b905e2305d2e946d621ce6ba73c16dbc1ed4c
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-22 06:30:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="image-ascii",
+    name="imageascii",
     version="0.1.0",
     author="tianhukj",
     author_email="tianhukj@outlook.com",
