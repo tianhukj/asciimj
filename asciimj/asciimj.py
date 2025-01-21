7ad964328146870be7ddf53f64652a38daa79222
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-22 07:28:42 +0800

    Update asciimj.py

--- asciimj/asciimj.py
+++ asciimj/asciimj.py
@@ -1,43 +1,32 @@
 from PIL import Image
 
-# ASCII characters used to build the output text
-ASCII_CHARS = "@%#*+=-:. "
-
-def resize_image(image, new_width=100):
+def convert_image_to_ascii(image, output_width=100):
+    """
+    将图像转换为 ASCII 艺术字符串。
+    
+    :param image: PIL.Image 对象
+    :param output_width: 输出 ASCII 艺术的宽度
+    :return: ASCII 艺术字符串
+    """
+    
+    # 将图像调整为指定宽度
     width, height = image.size
     aspect_ratio = height / width
-    new_height = int(aspect_ratio * new_width)
-    resized_image = image.resize((new_width, new_height))
-    return resized_image
-
-def grayify(image):
-    grayscale_image = image.convert("L")
-    return grayscale_image
-
-def pixels_to_ascii(image):
-    pixels = image.getdata()
-    ascii_str = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
-    return ascii_str
-
-def convert_image_to_ascii(image_path, new_width=100):
-    try:
-        image = Image.open(image_path)
-    except Exception as e:
-        print(e)
-        return None
+    new_height = int(output_width * aspect_ratio)
+    resized_image = image.resize((output_width, new_height))
     
-    image = resize_image(image, new_width)
-    image = grayify(image)
+    # 将图像转换为灰度图
+    grayscale_image = resized_image.convert("L")
     
-    ascii_str = pixels_to_ascii(image)
-    img_width = image.width
+    # 定义 ASCII 字符
+    ascii_chars = "@%#*+=-:. "
+    pixels = grayscale_image.getdata()
+    
+    # 将像素值映射到 ASCII 字符
+    ascii_str = "".join([ascii_chars[pixel // 32] for pixel in pixels])
+    
+    # 将 ASCII 字符串分行
     ascii_str_len = len(ascii_str)
-    ascii_img = "\n".join([ascii_str[index:(index + img_width)] for index in range(0, ascii_str_len, img_width)])
+    ascii_art = "\n".join([ascii_str[i:i+output_width] for i in range(0, ascii_str_len, output_width)])
     
-    return ascii_img
-
-if __name__ == "__main__":
-    import sys
-    path = sys.argv[1]
-    width = int(sys.argv[2]) if len(sys.argv) > 2 else 100
-    print(convert_image_to_ascii(path, width))
+    return ascii_art
